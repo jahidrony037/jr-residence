b5ef10330bf4d4e056764878e6865ed57a9f4764
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-04-09 19:37:38 +0600

    react-router and font family added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,5 +1,10 @@
+import { Outlet } from "react-router-dom";
 const Root = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./layouts/Root.jsx";
+import { router } from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Root />
+    <RouterProvider router={router}>
+      <Root />
+    </RouterProvider>
   </React.StrictMode>
 );
