631611ed0e03b21b8032ee1bb0f63d57ea56ca01
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-04-10 03:52:34 +0600

    email password verification code added and observer added

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,50 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import PropTypes from "prop-types";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase.init";
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUser = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: { name },
+      photoURL: { photo },
+    });
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+    });
+    return () => unSubscribe();
+  }, []);
   const allInfo = {
-    name: "jahid rony",
+    createUser,
+    loginUser,
+    logOut,
+    user,
+    updateUser,
   };
   return (
     <AuthContext.Provider value={allInfo}>{children}</AuthContext.Provider>
